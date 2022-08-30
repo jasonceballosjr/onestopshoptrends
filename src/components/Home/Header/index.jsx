df445f20c23228d517fa7fcaf932c0eea91518e0
Jason Ceballos <jason.ceballos.jr@gmail.com>
2022-08-30 01:14:31 -0500

    Aug 30 commit

--- src/components/Home/Header/index.jsx
+++ src/components/Home/Header/index.jsx
@@ -3,13 +3,12 @@ import './styles.css';
 
 const Header = () => (
   <header className='home-header'>
-    <h2>Inc. This Morning</h2>
+    <h2>One Stop Shop Trends</h2>
     <h1>
       <span>“</span> Blog <span>”</span>
     </h1>
     <p>
-      awesome place to make oneself <br /> productive and entertained through
-      daily updates.
+      The <strong> latest and trendiest</strong> <br /> products from Amazon, all in one place! 
     </p>
   </header>
 );
--- src/config/data.js
+++ src/config/data.js
@@ -1,13 +1,13 @@
 export const blogList = [
   {
     id: 1,
-    title: 'Welcome! ',
+    title: 'Welcome to One Stop Shop Trends! ',
     category: 'All',
     subCategory: ['All'],
     description:
       "Welcome to One Stop Shop Trends! Here, you can find the most trending items on the internet, whether it's from TikTok or Instagram. I hope that you find this website as a convenient One Stop Shop that you can periodically check in on. I will do my best to earn my way to your bookmark bar! If there are any inquiries or feedback you'd like to suggest, I'm always open and I'm constantly looking for ways to improve. With that, thank you for visiting and we hope to see you again!",
     authorName: 'Jason Ceballos',
-    authorAvatar: '/assets/images/jason.jpg',
+    authorAvatar: '/assets/images/jason.PNG',
     createdAt: 'August 30th, 2022',
     cover: '/assets/images/Logo.PNG',
   },
