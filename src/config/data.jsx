15ccc893c0722ae93fd3207e832a0f3a6022a920
Jason Ceballos <jason.ceballos.jr@gmail.com>
2022-08-30 23:29:18 -0500

    Sept 1 Commit

--- src/config/data.jsx
+++ src/config/data.jsx
@@ -7,7 +7,7 @@ export const blogList = [
     description:
       "Welcome to One Stop Shop Trends! Here, you can find the most trending items on the internet, whether it's from TikTok or Instagram. I hope that you find this website as a convenient One Stop Shop that you can periodically check in on. I will do my best to earn my way to your bookmark bar! If there are any inquiries or feedback you'd like to suggest, I'm always open and I'm constantly looking for ways to improve. With that, thank you for visiting and we hope to see you again!",
     authorName: 'Jason Ceballos',
-    authorAvatar: '/assets/images/jason.JPG',
+    authorAvatar: '/assets/images/jasonc.JPG',
     createdAt: 'August 30th, 2022',
     cover: '/assets/images/osstlogo.jpg',
   },
