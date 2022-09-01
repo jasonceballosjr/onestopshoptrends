8b689c579f7dfdb113c389e784cc5fdd1325e3d2
Jason Ceballos <jason.ceballos.jr@gmail.com>
2022-09-01 01:12:39 -0500

    Sept 1 Commit

--- src/config/data.jsx
+++ src/config/data.jsx
@@ -13,4 +13,17 @@ export const blogList = [
     cover: '/assets/images/backscrub-product1.jpg',
     bloglink: 'https://amzn.to/3Q4453k',
   },
+  {
+    id: 2,
+    title: 'HP Sprocket Instant Photo Printer!',
+    category: 'Phone',
+    subCategory: ['Accessories','TikTok','Technology','Phone'],
+    description:
+      "Running out of storage on your phone? Worry no more! HP Sprocket provides an instant photo printer that connects to your iOS/Android device. One of the most interesting features, in my opinion, is that it uses ZERO INK! With Over 11k views and rated at 4.5/5, this item is clearly a must have. To me, I find it to be an absolute pain getting pictures from my phone printed out. Pictures from any device really! You have to go to Walgreens or whatever local store is capable of printing pictures. Overtime, the cost of printing your pictures will become more than this device here! The link is under the title, check it out! Disclosure: Links contain affiliates. When you buy through one of our links we will receive a commission. This is at no cost to you. Thank you! We hope to have you again soon to check out the next latest and trendiest product. ",
+    authorName: 'Jason Ceballos',
+    authorAvatar: '/assets/images/jasonc.jpg',
+    createdAt: 'September 1st, 2022',
+    cover: '/assets/images/hpsprocket.jpg',
+    bloglink: 'https://amzn.to/3cENuFG',
+  },
 ];
